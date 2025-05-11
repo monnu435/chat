0a1913aeec0cb67e3db4405b977969d66265b005
monnu435 <cdongjun0514@gmail.com>
2025-05-11 17:56:38 +0900

    Add UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Sginup";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: (
+        <div>
+          {" "}
+          Root Layout <Outlet />{" "}
+        </div>
+      ),
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "auth",
+          children: [
+            {
+              path: "login",
+              element: <Login />,
+            },
+            {
+              path: "signup",
+              element: <Signup />,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "chat",
+      element: (
+        <div>
+          {" "}
+          Chat Layout <Outlet />{" "}
+        </div>
+      ),
+      children: [
+        {
+          path: "rooms",
+          element: <div>Chat Rooms Page</div>,
+        },
+        {
+          path: ":roomId",
+          element: <div>Chat Room Page</div>,
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
